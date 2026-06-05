0c11e13c038828cd303079fcc2d6b6f4cb86af92
Waridi318 <rosenjeri7151@gmail.com>
2026-06-06 00:27:53 +0300

    Test case fail on function move_txid. Corrected to reflect the exact expected format of return value

--- src/lib.rs
+++ src/lib.rs
@@ -102,7 +102,7 @@ pub fn apply_fee(balance: &mut u64, fee: u64) {
 
 pub fn move_txid(txid: String) -> String {
     //Return formatted string including the txid for display or logging
-    txid
+    format!("txid: {}",txid)
 }
 
 // TODO: Add necessary derive traits
