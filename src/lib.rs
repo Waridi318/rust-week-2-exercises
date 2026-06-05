0f2635a6bcbd80a3af1a0fe3ead9da6de603ff73
Waridi318 <rosenjeri7151@gmail.com>
2026-06-05 19:01:15 +0300

    Corrected syntax error in function: parse_satoshis, missing semicolon

--- src/lib.rs
+++ src/lib.rs
@@ -48,7 +48,7 @@ pub fn swap_endian_u32(num: u32) -> [u8; 4] {
 pub fn parse_satoshis(input: &str) -> Result<u64, String> {
     //Parse input string to u64, return error string if invalid
 
-    let result =  input.parse()
+    let result =  input.parse();
     match result{
         Ok(num) => Ok(num),
         Err(error) => Err(error.to_string()),
