01f5c3a7c55513a03e2be753c7f9f5d92d912c29
Waridi318 <rosenjeri7151@gmail.com>
2026-06-06 00:55:23 +0300

    Apply rustfmt formatting

--- src/lib.rs
+++ src/lib.rs
@@ -27,14 +27,12 @@ pub fn bytes_to_hex(bytes: &[u8]) -> String {
     //Implement conversion of bytes slice to hex string
 
     hex::encode(bytes)
-    
 }
 
 pub fn hex_to_bytes(hex: &str) -> Result<Vec<u8>, hex::FromHexError> {
     //Implement conversion of hex string to bytes vector
 
     hex::decode(hex)
-    
 }
 
 pub fn swap_endian_u32(num: u32) -> [u8; 4] {
